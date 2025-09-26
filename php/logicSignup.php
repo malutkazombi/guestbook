65d24d192c9eb02741dc3236ae06f5ac3bc294f4
malutkazombi <gothamexplode@gmail.com>
2025-09-26 11:47:23 +0900

    Signup Separation

---
+++ php/logicSignup.php
@@ -0,0 +1,34 @@
+<?php
+require_once '/var/www/config/pepper.php';
+require_once '/var/www/html/functions.php';
+if (isset($_POST['User_id']) && (isset($_POST['User_name'])) && (isset($_POST['password']))) {
+  try {
+    $db = new PDO('mysql:host=mysql;dbname=test;charset=utf8', 'test', 'test');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (validate_token(filter_input(INPUT_POST, 'token')) && isset($_POST['User_id']) && (isset($_POST['User_name'])) && (isset($_POST['password']))) {
+      $user_id = filter_input(INPUT_POST, 'User_id');
+      $user_name =  filter_input(INPUT_POST, 'User_name');
+      $password =  filter_input(INPUT_POST, 'password');
+
+      $pwd_peppered = hash_hmac("sha256", $password, $pepper);
+      $hashedPassword = password_hash($pwd_peppered, PASSWORD_ARGON2ID);
+
+      $sto = $db->prepare('INSERT INTO Users(user_id,user_name,password) VALUES (:user_id,:user_name,:password)');
+
+      $sto->bindValue(':user_id', $user_id);
+      $sto->bindValue(':user_name', $user_name);
+      $sto->bindValue(':password', $hashedPassword);
+
+      if ($sto->execute()) {
+        header("Location: login.php");
+        exit;
+      } else {
+        print("<p>SQL文実行時にエラーが発生しました</p>");
+      }
+    }
+  } catch (PDOException $e) {
+    die("<p>処理に失敗しました: " . $e->getMessage() . "</p>");
+  }
+}
+?>
